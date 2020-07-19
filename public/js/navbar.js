7931b989cb2780203c106a7880c425f2288852b0
Bertrand Bourion <bertrandbou@hotmail.fr>
2020-07-19 20:22:21 +0200

    changed: text in description at the bottom of the page

---
+++ public/js/navbar.js
@@ -0,0 +1,10 @@
+/* var nav = document.querySelector("#siteWrapper nav");
+
+function scrolled(){
+	var windowHeight = document.body.clientHeight,
+		currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	
+	nav.className = (currentScroll >= windowHeight - nav.offsetHeight) ? "fixed" : "";
+}
+
+addEventListener("scroll", scrolled, false); */
